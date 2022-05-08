9578956b76da57e844c0182ccaa3659e383be5a3
prashantmagare <prashantmagare3010@gmail.com>
2022-05-08 22:26:54 +0530

    Added: UC2_Part Time Emp wage

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace EmpWage
+﻿//UC3_Part Time Employee And Wage
+namespace EmpWage
 {
     internal class Program
     {
@@ -6,8 +7,10 @@
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
             //Constants
-            int IS_FULL_TIME = 1;
-            int Emp_RATE_PER_TIME = 20;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int Emp_RATE_PER_HOUR = 20;
+
             //Variables
             int empHrs = 0;
             int empwage = 0;
@@ -15,7 +18,12 @@
             Random random = new Random();
             //Computation
             int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
+
             {
                 Console.WriteLine("Employee is Present");
                 empHrs = 8;
@@ -25,7 +33,8 @@
                 Console.WriteLine("Employee is Absent");
                 empHrs = 0;
             }
-            empwage = empHrs * Emp_RATE_PER_TIME;
+            empwage = empHrs * Emp_RATE_PER_HOUR;
+
             Console.WriteLine("EmpWage : " + empwage);
         }
     }
