26d4f965f1191aa7eba2f409e0dd279a768b929d
prashantmagare <prashantmagare3010@gmail.com>
2022-05-08 22:13:50 +0530

    Added: UC1

---
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace EmpWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            //Constants
+            int IS_FULL_TIME = 1;
+            //It is using to generate random numbers
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
