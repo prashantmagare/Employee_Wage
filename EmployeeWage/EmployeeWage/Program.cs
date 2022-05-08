fb16c3703acc0f1dd81ff726d6162870f996b962
prashantmagare <prashantmagare3010@gmail.com>
2022-05-08 22:20:55 +0530

    Added: UC2_Daily Emp wage

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -7,6 +7,10 @@
             Console.WriteLine("Welcome to Employee Wage Computation Program");
             //Constants
             int IS_FULL_TIME = 1;
+            int Emp_RATE_PER_TIME = 20;
+            //Variables
+            int empHrs = 0;
+            int empwage = 0;
             //It is using to generate random numbers
             Random random = new Random();
             //Computation
@@ -14,11 +18,15 @@
             if (empCheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empwage = empHrs * Emp_RATE_PER_TIME;
+            Console.WriteLine("EmpWage : " + empwage);
         }
     }
 }
