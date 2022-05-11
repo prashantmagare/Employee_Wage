caaec1e2f9839ada973b1a6f02a369cbdc9a55b8
prashantmagare <prashantmagare3010@gmail.com>
2022-05-11 18:32:34 +0530

    Added:UC4_SwitchCase

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -1,41 +1,45 @@
-﻿//UC3_Part Time Employee And Wage
+﻿using System;
+//Switch Case 
 namespace EmpWage
 {
     internal class Program
     {
+
+        //Constants
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int Emp_RATE_PER_TIME = 20;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
-            //Constants
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int Emp_RATE_PER_HOUR = 20;
-
             //Variables
             int empHrs = 0;
             int empwage = 0;
+
             //It is using to generate random numbers
             Random random = new Random();
             //Computation
             int empCheck = random.Next(0, 2);
-            if (empCheck == IS_PART_TIME)
-            {
-                empHrs = 4;
-            }
-            else if (empCheck == IS_FULL_TIME)
 
+            switch (empCheck)
             {
-                Console.WriteLine("Employee is Present");
-                empHrs = 8;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-                empHrs = 0;
-            }
-            empwage = empHrs * Emp_RATE_PER_HOUR;
+                case IS_PART_TIME:
+                    Console.WriteLine("Employee is present");
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    Console.WriteLine("Employee is also present");
+                    empHrs = 8;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    empHrs = 0;
+                    break;
 
+            }
+
+            empwage = empHrs * Emp_RATE_PER_TIME;
             Console.WriteLine("EmpWage : " + empwage);
         }
+
     }
 }
