3ca70c3979d19f543ed933dd987519e26a19d392
prashantmagare <prashantmagare3010@gmail.com>
2022-05-11 19:33:09 +0530

    Added: UC6_total Emp Wage for 100 Hrs

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -8,15 +8,23 @@ namespace EmpWage
         //Constants
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
-        public const int Emp_RATE_PER_TIME = 20;
+        public const int Emp_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
+        public const int days_in_month = 20;
         static void Main(string[] args)
         {
             //Variables
             int empHrs = 0;
-            int empwage = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
 
-            //It is using to generate random numbers
-            Random random = new Random();
+
+            //Computation
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            {
+                //It is using to generate random numbers
+                Random random = new Random();
             //Computation
             int empCheck = random.Next(0, 3);
 
@@ -36,9 +44,19 @@ namespace EmpWage
                     break;
 
             }
+               
 
-            empwage = empHrs * Emp_RATE_PER_TIME;
-            Console.WriteLine("EmpWage : " + empwage);
+                    totalEmpHrs += empHrs;
+                Console.WriteLine("Days : " + totalWorkingDays + " Emp Hrs : " + empHrs);
+              
+            }
+
+            
+                Console.WriteLine("Total Employee Hrs is " + totalEmpHrs);
+
+                int totalEmpWage = totalEmpHrs * Emp_RATE_PER_HOUR;
+                Console.WriteLine("Total Employee Wage : " + totalEmpWage);
+            
         }
 
     }
