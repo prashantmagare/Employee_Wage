5023cb3ba7f1f3c2a838e10c5427cfa552df0299
prashantmagare <prashantmagare3010@gmail.com>
2022-05-17 23:19:29 +0530

    Added: Compute Emp Wage For Multiple Companies

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -2,27 +2,24 @@
 //Switch Case 
 namespace EmpWage
 {
-    internal class Program
+    public class Program
     {
 
         //Constants
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
-        public const int Emp_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 20;
-        public const int MAX_HRS_IN_MONTH = 100;
-        public const int days_in_month = 20;
-        static void ComputeWage()
+      public static int CompanyName(string companyName, int empRatePerHr, int noOfWorkingDays, int maxHrPerMonth )
         {
             //Variables
-            int empHrs = 0;
-            int totalEmpHrs = 0;
+            
+            int totalEmpHrs = 0; 
             int totalWorkingDays = 0;
+           
+            while (totalEmpHrs < maxHrPerMonth && totalWorkingDays < noOfWorkingDays)
+           {
+                int empHrs = 0;
+                totalWorkingDays++;
 
-
-            //Computation
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
-            {
                 //It is using to generate random numbers
                 Random random = new Random();
             //Computation
@@ -44,23 +41,22 @@ namespace EmpWage
                     break;
 
             }
-               
 
                     totalEmpHrs += empHrs;
                 Console.WriteLine("Days : " + totalWorkingDays + " Emp Hrs : " + empHrs);
               
             }
 
-            
                 Console.WriteLine("Total Employee Hrs is " + totalEmpHrs);
 
-                int totalEmpWage = totalEmpHrs * Emp_RATE_PER_HOUR;
+                int totalEmpWage = totalEmpHrs * empRatePerHr;
                 Console.WriteLine("Total Employee Wage : " + totalEmpWage);
-            
+            return totalEmpWage;
         }
          static void Main(string[] args)
         {
-            ComputeWage();
+            CompanyName("Dmart", 25, 20, 80);
+            CompanyName("Reliance", 10, 4, 20);
         }
 
     }
